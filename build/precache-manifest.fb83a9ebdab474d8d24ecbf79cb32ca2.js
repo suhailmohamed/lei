20bb2a19a5bebfbaea0e1f1265cc4b2f613e5807
suhailmohamed <suhailmohamed333@gmail.com>
2020-04-10 17:21:20 +0530

    Add build folder
    Add build folder

---
+++ build/precache-manifest.fb83a9ebdab474d8d24ecbf79cb32ca2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57ac678c4f2dce1a9dfe",
+    "url": "/lei/static/css/main.1a71ae6c.chunk.css"
+  },
+  {
+    "revision": "57ac678c4f2dce1a9dfe",
+    "url": "/lei/static/js/main.57ac678c.chunk.js"
+  },
+  {
+    "revision": "fe28a68686d1526d855b",
+    "url": "/lei/static/js/runtime~main.fe28a686.js"
+  },
+  {
+    "revision": "9b7554f93f61b751f189",
+    "url": "/lei/static/js/2.9b7554f9.chunk.js"
+  },
+  {
+    "revision": "beec063a7454d784c64310a6aab290f3",
+    "url": "/lei/index.html"
+  }
+];
